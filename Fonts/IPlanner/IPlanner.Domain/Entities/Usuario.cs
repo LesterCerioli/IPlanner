541baf8c79faf83678afa31bae7dcf0ac809beea
LesterCerioli <lesterlucasit@hotmail.com>
2020-04-28 10:39:48 -0300

    Initial Repository- Repositório Inicial

---
+++ Fonts/IPlanner/IPlanner.Domain/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPlanner.Domain.Entities
+{
+    public class Usuario
+    {
+    }
+}
---
+++ Fonts/IPlanner/IPlanner.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IPlanner.Repository
+{
+    public class Class1
+    {
+    }
+}
