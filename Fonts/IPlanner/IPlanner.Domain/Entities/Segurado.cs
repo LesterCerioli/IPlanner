27aae9750d85b6912c35dffa44fb8e3751b3b0e7
LesterCerioli <lesterlucasit@hotmail.com>
2020-04-29 09:52:46 -0300

    Estruutura primária de clkasses de Entidades de Domínio-Primary Enentities Domain Structure

---
+++ Fonts/IPlanner/IPlanner.Domain/Entities/Segurado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPlanner.Domain.Entities
+{
+    public class Segurado : Entity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeMeio { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public string Idade { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usurio Usuario { get; set; }
+
+
+
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(Nome))
+                AddCritical("O Nome deve ser informado!");
+
+            if (string.IsNullOrEmpty(NomeMeio))
+                AddCritical("O Nome do Meio deve ser informado!");
+
+            if (string.IsNullOrEmpty(SobreNome))
+                AddCritical("O SobreNome deve ser informado!");
+
+            if (string.IsNullOrEmpty(Email))
+                AddCritical("O Email deve ser informado!");
+
+
+        }
+    }
+}
--- Fonts/IPlanner/IPlanner.Domain/Entities/Usuario.cs
+++ Fonts/IPlanner/IPlanner.Domain/Entities/Usuario.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace IPlanner.Domain.Entities
 {
-    public class Usuario
+    public class Usuario : Entity
     {
+        public int Id {get; set;}
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public virtual ICollection<Segurado> Segurados { get; set; }
+
+
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(Email))
+                AddCritical("O Email deve ser inoformado!");
+        }
     }
 }
