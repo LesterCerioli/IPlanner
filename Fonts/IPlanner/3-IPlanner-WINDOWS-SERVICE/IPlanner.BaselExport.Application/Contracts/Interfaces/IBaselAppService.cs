87877114fed137fe0f1bc770a6eea3c0b0fdc8ce
Lester Cerioli <lesterlucasit@hotmail.com>
2020-09-19 17:42:08 -0300

    Implementing Windows Service

---
+++ Fonts/IPlanner/3-IPlanner-WINDOWS-SERVICE/IPlanner.BaselExport.Application/Contracts/Interfaces/IBaselAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPlanner.BaselExport.Application.Contracts.Interfaces
+{
+    public interface IBaselAppService : IDisposable
+    {
+        Task<ResponseMessage<BaselExportResponse>> ExportAsync();
+    }
+}
---
+++ Fonts/IPlanner/3-IPlanner-WINDOWS-SERVICE/IPlanner.BaselExport.Application/Contracts/RequestResponse/BaselExportResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPlanner.BaselExport.Application.Contracts.RequestResponse
+{
+    public sealed class BaselExportResponse
+    {
+    }
+}
